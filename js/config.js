e5b46b2ced783229d2b2f5448dd4120daf2abdf9
thoompranay-maker <thoompranay@gmail.com>
2026-09-13 23:26:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
 
+const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
+const SUPABASE_PUBLISHABLE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
